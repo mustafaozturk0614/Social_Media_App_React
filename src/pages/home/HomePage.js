efd7a6c6b8996691c237ae1cb8f9c7201f9db6f2
musty1406 <musty1406@gmail.com>
2022-11-24 12:27:56 +0300

    logout yapıldı

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,17 +1,27 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Feed from "../../component/feed/Feed";
 import Topbar from "../../component/topbar/Topbar";
+import { findbyTokenwithAxios } from "../../store/features/UserSlice";
 
 function HomePage() {
   const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.userProfile);
+  const getUser = async () => {
+    await dispatch(findbyTokenwithAxios(token)).then(console.log(user));
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
     <>
       <Topbar></Topbar>
-
       <div className="homeContainer">
-        <div>Left bar</div>
-        <div> akış </div>
+        <div>{user.username}</div>
+        <Feed></Feed>
         <div>right bar</div>
       </div>
     </>
